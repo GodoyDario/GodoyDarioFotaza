ad9fa6ab903f26f247fa7c53ea542b1626d1612e
GodoyDario <dariogodoy0896@gmail.com>
2026-06-08 20:50:32 -0300

    separado de login y registrar (hay que arreglar el css y el layout para que se vea bien)

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # APP FOTAZA 
 
 Es una aplicacion donde usuarios pueden subir imagenes, valorizar, comentar, denunciar.
+
+## Guia de uso
+ - Instale las dependencias con `npm install` 
+ - Use `npm run dev` para iniciar en modo watch, para poder editar y recargar el servidor
+
+ ## Usuario de prueba
+ - Usuario: user
+ - Constraseña: user123
+
--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 import 'dotenv/config';
 import express from "express";
 const app = express();
+import path from "path";
+
 
 //Contantes
 const PORT = process.env.PORT;
 const HOST = process.env.HOST || "localhost";
 
+
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static( "public"))
 
 //Motor de plantillas
 app.set("view engine", "pug");
@@ -20,6 +24,9 @@ app.get("/", (req, res) => {
 });
 app.get("/login", (req, res) => {
     res.render("login", { title: "Iniciar Sesión" });
+});
+app.get("/register", (req, res) => {
+    res.render("register", { title: "Registrar Usuario" });
 });
 
 //Servidor
